a46e7a2812a4c2cd395a4b9121a409d84276c4de
Ryan DV <rdevilla@uwaterloo.ca>
2014-10-18 20:41:12 -0400

    Separate Base and FenParser module

---
+++ src/Chess/Base.hs
@@ -0,0 +1,120 @@
+module Chess.Base
+  ( CastleRights(..),
+    Coordinate(..),
+    File,
+    Piece(..),
+    PieceType(..),
+    Player(..),
+    Rank,
+    RegularBoardRepresentation,
+    RegularGame(..),
+    Square(..),
+
+    pseudoLegalMoves
+  ) where
+
+data Square = Square
+  { pieceOn  :: Maybe Piece
+  , location :: Coordinate
+  } deriving(Read, Show)
+
+data Piece = Piece
+  { pieceType  :: PieceType
+  , pieceOwner :: Player
+  } deriving(Read)
+
+instance Show Piece where
+  show (Piece Rook White)   = "R"
+  show (Piece Knight White) = "N"
+  show (Piece Bishop White) = "B"
+  show (Piece Queen White)  = "Q"
+  show (Piece King White)   = "K"
+  show (Piece Pawn White)   = "P"
+
+  show (Piece Rook Black)   = "r"
+  show (Piece Knight Black) = "n"
+  show (Piece Bishop Black) = "b"
+  show (Piece Queen Black)  = "q"
+  show (Piece King Black)   = "k"
+  show (Piece Pawn Black)   = "p"
+
+data PieceType  = Pawn | Knight | Bishop | Rook | Queen | King deriving (Enum, Eq, Read)
+data Player = White | Black deriving (Enum, Eq, Read, Show)
+
+data Coordinate = Coordinate Rank File deriving(Read, Show)
+type Rank   = Char
+type File   = Integer
+
+-- KkQq
+data CastleRights = CastleRights Bool Bool Bool Bool deriving(Show)
+
+type RegularBoardRepresentation   = [[Square]]
+
+data RegularGame = RegularGame
+  { placement       :: RegularBoardRepresentation
+  , activeColor     :: Player
+  , castlingRights  :: CastleRights
+  , enPassantSquare :: Maybe Coordinate
+  , halfMoveClock   :: Integer
+  , fullMoveNumber  :: Integer
+  }
+
+instance Show RegularGame where
+  show g =    "\n"
+           ++ "  abcdefgh \n"
+           ++ "8 " ++ concatMap (maybe "-" show) eighthRank  ++ " 8\n"
+           ++ "7 " ++ concatMap (maybe "-" show) seventhRank ++ " 7\n"
+           ++ "6 " ++ concatMap (maybe "-" show) sixthRank   ++ " 6\n"
+           ++ "5 " ++ concatMap (maybe "-" show) fifthRank   ++ " 5\n"
+           ++ "4 " ++ concatMap (maybe "-" show) fourthRank  ++ " 4\n"
+           ++ "3 " ++ concatMap (maybe "-" show) thirdRank   ++ " 3\n"
+           ++ "2 " ++ concatMap (maybe "-" show) secondRank  ++ " 2\n"
+           ++ "1 " ++ concatMap (maybe "-" show) firstRank   ++ " 1\n"
+           ++ "  abcdefgh \n " where
+    eighthRank  = map pieceOn $ placement g !! 7
+    seventhRank = map pieceOn $ placement g !! 6
+    sixthRank   = map pieceOn $ placement g !! 5
+    fifthRank   = map pieceOn $ placement g !! 4
+    fourthRank  = map pieceOn $ placement g !! 3
+    thirdRank   = map pieceOn $ placement g !! 2
+    secondRank  = map pieceOn $ placement g !! 1
+    firstRank   = map pieceOn $ head $ placement g
+
+pseudoLegalMoves                   :: RegularGame -> [(Coordinate, Coordinate)]
+pseudoLegalMoves RegularGame { placement = b
+                             , enPassantSquare = e
+                             } = (concatMap . concatMap) (pseudoLegalMovesFrom e) b where
+
+pseudoLegalMovesFrom :: Maybe Coordinate -> Square -> [(Coordinate, Coordinate)]
+pseudoLegalMovesFrom _ (Square Nothing _)            = []
+pseudoLegalMovesFrom c (Square (Just (Piece p _)) l) | p == Pawn   = pseudoLegalPawnMoves c l
+                                                     | p == Knight = pseudoLegalKnightMoves l
+                                                     | p == Bishop = pseudoLegalBishopMoves l
+                                                     | p == Rook   = pseudoLegalRookMoves l
+                                                     | p == Queen  = pseudoLegalQueenMoves l
+                                                     | p == King   = pseudoLegalKingMoves l
+
+
+pseudoLegalPawnMoves                          :: Maybe Coordinate -> Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalPawnMoves Nothing c                = standardPawnMoves c
+pseudoLegalPawnMoves (Just enPassant) c = standardPawnMoves c ++ enPassantMoves enPassant where
+  enPassantMoves  :: Coordinate -> [(Coordinate, Coordinate)]
+  enPassantMoves  = undefined
+
+standardPawnMoves :: Coordinate -> [(Coordinate, Coordinate)]
+standardPawnMoves = undefined
+
+pseudoLegalKnightMoves   :: Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalKnightMoves   = undefined
+
+pseudoLegalBishopMoves   :: Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalBishopMoves   = undefined
+
+pseudoLegalRookMoves   :: Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalRookMoves   = undefined
+
+pseudoLegalQueenMoves   :: Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalQueenMoves   = undefined
+
+pseudoLegalKingMoves   :: Coordinate -> [(Coordinate, Coordinate)]
+pseudoLegalKingMoves   = undefined
