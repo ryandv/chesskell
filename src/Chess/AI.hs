b0fef9f6922cf5ecd9dae36d3d685368bf5e4c7b
Ryan DV <rdevilla@uwaterloo.ca>
2015-01-24 18:40:09 -0500

    Make AI operate on explicit GameTrees

--- src/Chess/AI.hs
+++ src/Chess/AI.hs
@@ -7,16 +7,18 @@ import Chess.MoveGen
 
 import Control.Applicative
 
+import Data.List
 import Data.Maybe
+import Data.Ord
 
-data GameTree = GameTree
-  { gameTreeValue    :: RegularGame
-  , gameTreeChildren :: [GameTree]
-  , gameTreeLastMove :: Move
-  } | GameTreeRoot
-  { gameTreeValue    :: RegularGame
-  , gameTreeChildren :: [GameTree]
-  }
+data GameTree a = GameTree
+  { gameTreeValue    :: a
+  , gameTreeChildren :: [GameTree a]
+  , gameTreeLastMove :: Maybe Move
+  } deriving(Show)
+
+instance Functor GameTree where
+  fmap f (GameTree v children move) = GameTree (f v) ((fmap . fmap) f children) move
 
 evalGame      :: RegularGame -> Int
 evalGame game | isCheckmate game (activeColor game) = checkmateValue
@@ -43,13 +45,25 @@ evalPosition position = foldr ((+) . pieceValue) 0 $ concat position where
                                                     | otherwise      = -900
   pieceValue _ = 0
 
-gamesFrom      :: RegularGame -> [(Move, RegularGame)]
-gamesFrom game = mapMaybe (\move -> (move,) <$> makeMoveFrom game move) (pseudoLegalMoves game)
+-- Inspired by John Hughes' "Why Functional Programming Matters"
 
-gameTreeFrom          :: (RegularGame -> [RegularGame]) -> Move -> RegularGame -> GameTree
-gameTreeFrom f _ game = GameTreeRoot game (map (uncurry (gameTreeFrom f)) (gamesFrom game))
+gamesFrom      :: RegularGame -> [(Maybe Move, RegularGame)]
+gamesFrom game = mapMaybe (\move -> (Just move,) <$> makeMoveFrom game move) (pseudoLegalMoves game)
 
+gameTreeFrom          :: (RegularGame -> [RegularGame]) -> Maybe Move -> RegularGame -> GameTree RegularGame
+gameTreeFrom f move game = GameTree game (map (uncurry (gameTreeFrom f)) (gamesFrom game)) move
 
---decideOnMove :: Player -> RegularGame -> Move
---decideOnMove player game | player == White = fst $ miniMax 2 player Nothing game
---                         | otherwise = fst $ miniMax 2 player Nothing game
+minimize :: GameTree RegularGame -> GameTree RegularGame
+minimize (GameTree v [] move) = GameTree v [] move
+minimize (GameTree v children move) = minimumBy (comparing (evalGame . gameTreeValue)) (map maximize children)
+
+maximize :: GameTree RegularGame -> GameTree RegularGame
+maximize (GameTree v [] move) = GameTree v [] move
+maximize (GameTree v children move) = maximumBy (comparing (evalGame . gameTreeValue)) (map minimize children)
+
+prune 0 (GameTree v _ move) = GameTree v [] move
+prune n (GameTree v children move) = GameTree v (map (prune $ n - 1) children) move
+
+decideOnMove :: Player -> RegularGame -> Move
+decideOnMove player game | player == White = fromJust $ gameTreeLastMove $ maximize $ prune 3 $ gameTreeFrom (mapMaybe (makeMoveFrom game) . pseudoLegalMoves) Nothing game
+                         | otherwise = fromJust $ gameTreeLastMove $ minimize $ prune 3 $ gameTreeFrom (mapMaybe (makeMoveFrom game) . pseudoLegalMoves) Nothing game
